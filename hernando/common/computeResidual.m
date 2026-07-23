ec4b3cedfd9f83ded4dbaa8e5b21a484b5faa44c
Jacob Degitz <jacobdegitz98@gmail.com>
2026-07-23 14:20:31 -0500

    Cleanup functions & descriptions

--- hernando/common/computeResidual.m
+++ hernando/common/computeResidual.m
@@ -18,19 +18,10 @@
 %      - algoParams.species(2).frequency = [3.80, 3.40, 2.60, 1.94, 0.39, -0.60]
 %      - algoParams.species(2).relAmps = [0.087 0.693 0.128 0.004 0.039 0.048]
 % 
-%   - algoParams.size_clique = 1; % Size of MRF neighborhood (1 uses an 8-neighborhood, common in 2D)
 %   - algoParams.range_r2star = [0 0]; % Range of R2* values
 %   - algoParams.NUM_R2STARS = 1; % Numbre of R2* values for quantization
 %   - algoParams.range_fm = [-400 400]; % Range of field map values
 %   - algoParams.NUM_FMS = 301; % Number of field map values to discretize
-%   - algoParams.NUM_ITERS = 40; % Number of graph cut iterations
-%   - algoParams.SUBSAMPLE = 2; % Spatial subsampling for field map estimation (for speed)
-%   - algoParams.DO_OT = 1; % 0,1 flag to enable optimization transfer descent (final stage of field map estimation)
-%   - algoParams.LMAP_POWER = 2; % Spatially-varying regularization (2 gives ~ uniformn resolution)
-%   - algoParams.lambda = 0.05; % Regularization parameter
-%   - algoParams.LMAP_EXTRA = 0.05; % More smoothing for low-signal regions
-%   - algoParams.TRY_PERIODIC_RESIDUAL = 0; % Take advantage of periodic residual if uniform TEs (will change range_fm)  
-%   - algoParams.residual: in case we pre-computed the fit residual (mostly for testing) 
 %
 % Returns: 
 %  - residual: the residual, of size NUM_FMS X sx X sy
--- hernando/graphcut/graphCutIterations.m
+++ hernando/graphcut/graphCutIterations.m
@@ -20,17 +20,10 @@
 %      - algoParams.species(2).relAmps = [0.087 0.693 0.128 0.004 0.039 0.048]
 % 
 %   - algoParams.size_clique = 1; % Size of MRF neighborhood (1 uses an 8-neighborhood, common in 2D)
-%   - algoParams.range_r2star = [0 0]; % Range of R2* values
-%   - algoParams.NUM_R2STARS = 1; % Numbre of R2* values for quantization
 %   - algoParams.range_fm = [-400 400]; % Range of field map values
 %   - algoParams.NUM_FMS = 301; % Number of field map values to discretize
 %   - algoParams.NUM_ITERS = 40; % Number of graph cut iterations
-%   - algoParams.SUBSAMPLE = 2; % Spatial subsampling for field map estimation (for speed)
-%   - algoParams.DO_OT = 1; % 0,1 flag to enable optimization transfer descent (final stage of field map estimation)
-%   - algoParams.LMAP_POWER = 2; % Spatially-varying regularization (2 gives ~ uniformn resolution)
 %   - algoParams.lambda = 0.05; % Regularization parameter
-%   - algoParams.LMAP_EXTRA = 0.05; % More smoothing for low-signal regions
-%   - algoParams.TRY_PERIODIC_RESIDUAL = 0; % Take advantage of periodic residual if uniform TEs (will change range_fm)  
 %   - algoParams.residual: in case we pre-computed the fit residual (mostly for testing) 
 %
 %   - residual: the fit residual, of size NUM_FMS X sx X sy 
@@ -86,10 +79,10 @@ numLocalMin = size(resLocalMinima,1);
 stepoffset = [0:(sx*sy-1)]'*numLocalMin;
 ercurrent = 1e10;
 clear cur_ind2
-% $$$ [A] = createExpansionGraphVARPRO_fast( residual, dfm, ones(sx,sy), algoParams.size_clique,ones(sx,sy), ones(sx,sy));
 
 % Main graph-cut loop. At each iteration, a max-flow/min-cut problem is solved
 fm = zeros(sx,sy);
+fmiters = zeros(sx,sy,algoParams.NUM_ITERS);
 for kg = 1:algoParams.NUM_ITERS
 
     fmiters(:,:,kg) = fm;
@@ -113,7 +106,7 @@ for kg = 1:algoParams.NUM_ITERS
         repCurInd = repmat(cur_ind2,[numLocalMin,1,1]);
         if cur_sign > 0
             stepLocator = (repCurInd+20/dfm>=resLocalMinima) & (resLocalMinima>0);
-            stepLocator = squeeze(sum(stepLocator,1))+1 ;
+            stepLocator = squeeze(sum(stepLocator,1))+1;
             validStep = masksignal>0 & stepLocator<=numMinimaPerVoxel;
         else
             stepLocator = (repCurInd-20/dfm>resLocalMinima) & (resLocalMinima>0);
@@ -139,22 +132,22 @@ for kg = 1:algoParams.NUM_ITERS
 
         % DH* 100309: fix errors where fm goes beyond range
         if cur_sign > 0
-            cur_step(nextValue(:)>length(fms)) = length(fms) - cur_ind(nextValue(:)>length(fms));
+            cur_step(nextValue(:)>algoParams.NUM_FMS) = algoParams.NUM_FMS - cur_ind(nextValue(:)>algoParams.NUM_FMS);
         else
             cur_step(nextValue(:)<1) = 1 - cur_ind(nextValue(:)<1);
         end
 
-        if DEBUG == 1
+        if DEBUG
             all_jump %#ok<NOPRT>
         end
     end
 
     % Create the huge adjacency matrix
-  if norm(lambdamap,'fro') > 0
+  % if norm(lambdamap,'fro') > 0
     [A] = createExpansionGraphVARPRO_fast( residual, dfm, lambdamap, algoParams.size_clique,cur_ind, cur_step);
-  else 
-    [A] = createExpansionGraphVARPRO_fast( residual, dfm, lambdamap, algoParams.size_clique,cur_ind, cur_step);
-  end
+  % else 
+  %   [A] = createExpansionGraphVARPRO_fast( residual, dfm, lambdamap, algoParams.size_clique,cur_ind, cur_step);
+  % end
     A(A<0) = 0;
 
     % Solve the max-flow/min-cut problem (max_flow is a function of the matlabBGL library)
@@ -166,35 +159,29 @@ for kg = 1:algoParams.NUM_ITERS
     cut1 = (cut_TS==-1);
     cut1b = 0*cut1 + 1;
     cut1b(end) = 0;
-    if DEBUG == 1
+    if DEBUG
         compareGC = [sum(sum(A(cut1b==1, cut1b==0))),  sum(sum(A(cut1==1, cut1==0)))] %#ok<NOPRT>
     end
 
     if sum(sum(A(cut1b==1, cut1b==0))) <= sum(sum(A(cut1==1, cut1==0)))
         cur_indST = cur_ind;
 
-        if DEBUG == 1
-            disp('Not taken');
-        end
+        if DEBUG, disp('Not taken'); end
     else
         cut = reshape(cut1(2:end-1)==0,sx,sy);
         cur_indST = cur_ind + cur_step.*(cut);
-        erST = sum(sum(residual(cur_indST(:)+resoffset(:)))) + dfm^2*lambdamap(1,1)*sum(sum(abs(diff(cur_indST,2,1)).^2)) + dfm^2*lambdamap(1,1)*sum(sum(abs(diff(cur_indST,2,2)).^2));
-        if DEBUG == 1
-            disp('Taken');
-            % $$$   pixelschanged = sum(sum(cut));
-        end
+        erST = sum(sum(residual(cur_indST(:)+resoffset(:)))) + ...
+            dfm^2*lambdamap(1,1)*sum(sum(abs(diff(cur_indST,2,1)).^2)) + ...
+            dfm^2*lambdamap(1,1)*sum(sum(abs(diff(cur_indST,2,2)).^2));
+        if DEBUG, disp('Taken'); end
     end
 
     % Update the field map (described as a map of indices in this function)
     prev_ind = cur_ind;
     cur_ind = cur_indST;
 
-    % $$$   size(fms)
-    % $$$   mymax = max(cur_ind(:))
-
     cur_ind(cur_ind<1) = 1;
-    cur_ind(cur_ind>length(fms)) = length(fms);
+    cur_ind(cur_ind>algoParams.NUM_FMS) = algoParams.NUM_FMS;
 
     fm = fms(cur_ind);
   
